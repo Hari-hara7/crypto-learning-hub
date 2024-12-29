54af5dfc74c1f786a83ceebffd8d7aa90fc19979
Your Name <Hariharanath247@gmail.com>
2024-12-29 12:23:00 +0530

    hari

--- crypto-learning-hub/app/page.tsx
+++ crypto-learning-hub/app/page.tsx
@@ -78,7 +78,7 @@ export default function HomePage() {
           </motion.div>
         </div>
 
-        {/* Crypto Cards Section */}
+        {/* Crypto Cards Section jj*/}
         <motion.div
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
           initial="hidden"
